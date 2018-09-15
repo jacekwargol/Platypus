5b40dd3efacff6b11419f412648f4f8e971a809d
Jacek Wargol <jwargol@gmail.com>
2018-09-15 22:07:32 +0200

    equality operators for Token

--- SharpTypus.Tests/LexerTests.cs
+++ SharpTypus.Tests/LexerTests.cs
@@ -7,6 +7,17 @@ using static SharpTypus.Parsing.TokenType;
 namespace SharpTypus.Tests {
     public class LexerTests {
         [Fact]
+        public void CanTokenizeInteger() {
+            var i = "123";
+            var expected = new Token(Integer, i, 1);
+
+            var lexer = new Lexer(i);
+            var actual = lexer.Tokenize()[0];
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void CanTokenizeSimpleExpression() {
             var addition = "1 + 2";
             var expected = new List<Token>() {
--- SharpTypus/Parsing/Token.cs
+++ SharpTypus/Parsing/Token.cs
@@ -3,19 +3,27 @@
 namespace SharpTypus.Parsing {
     class Token {
         public TokenType Type { get; }
-
-        private readonly String lexeme;
-        private readonly int line;
-
+        public string Lexeme { get; }
+        public int Line { get; }
 
         public Token(TokenType type, string lexeme, int line) {
             Type = type;
-            this.lexeme = lexeme;
-            this.line = line;
+            Lexeme = lexeme;
+            Line = line;
         }
 
 
-        public override string ToString() => lexeme;
-            //"Line " + line + ": " +  lexeme + ": " + Type;
+        public override string ToString() => Lexeme;
+
+        public static bool operator ==(Token left, Token right) =>
+            left.Type == right.Type && left.Lexeme == right.Lexeme;
+
+        public static bool operator !=(Token left, Token right) =>
+            !(left == right);
+
+        public override bool Equals(object obj) => obj is Token ? (Token)obj == this : false;
+
+        public override int GetHashCode() => (Type, Lexeme, Line).GetHashCode();
+
     }
 }
