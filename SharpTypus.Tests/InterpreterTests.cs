5cc9da2d963d5121f4b4cd5bdb98474a18f23a5a
Jacek Wargol <jwargol@gmail.com>
2018-09-17 23:42:10 +0200

    Tests for Unary, Addition

--- SharpTypus.Tests/InterpreterTests.cs
+++ SharpTypus.Tests/InterpreterTests.cs
@@ -27,5 +27,53 @@ namespace SharpTypus.Tests {
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void CanInterpretMinusUnary() {
+            var expr = new Unary(new Literal(new Token(Float, "2.1", 1)), new Token(Minus, "-", 1));
+            var expected = -2.1;
+
+            var interpreter = new Interpreter();
+            var actual = interpreter.Visit(expr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanInterpretBangUnary() {
+            var expr = new Unary(new Literal(new Token(False, "false", 1)), new Token(Bang, "!", 1));
+            var expected = true;
+
+            var interpreter = new Interpreter();
+            var actual = interpreter.Visit(expr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanInterpretIntFloatAddition() {
+            var expr = new Binary(new Literal(new Token(Integer, "3", 1)), new Literal(new Token(Float, "2.1", 1)),
+                new Token(Plus, "+", 1));
+
+            var expected = 5.1;
+
+            var interpreter = new Interpreter();
+            var actual = interpreter.Visit(expr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanInterpretComplexAddition() {
+            var right = new Binary(new Literal(new Token(Float, "2.0", 1)), new Literal(new Token(Integer, "2", 1)), new Token(Star, "*", 1));
+            var expr = new Binary(new Literal(new Token(Integer, "1", 1)), right, new Token(Plus, "+", 1));
+
+            var expected = 5.0;
+
+            var interpreter = new Interpreter();
+            var actual = interpreter.Visit(expr);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
