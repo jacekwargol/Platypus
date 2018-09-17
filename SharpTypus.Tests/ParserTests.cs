ba7cfd0219ecfe6fed9fdf2fcd527cd494c8ed30
Jacek Wargol <jwargol@gmail.com>
2018-09-17 13:34:48 +0200

    tests for binary exprs

--- SharpTypus.Tests/ParserTests.cs
+++ SharpTypus.Tests/ParserTests.cs
@@ -51,5 +51,50 @@ namespace SharpTypus.Tests {
             var actual = parser.Parse();
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void CanParseSimpleMultiplicativeExpression() {
+            var floatToken = new Token(Float, "1.0", 1);
+            var opToken = new Token(Star, "*", 1);
+            var tokens = new List<Token>() {
+                floatToken,
+                opToken,
+                floatToken
+            };
+
+            var expected = new Binary(new Literal(floatToken), new Literal(floatToken), opToken);
+
+            var parser = new Parser(tokens);
+            var actual = parser.Parse();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanParseComplexBinaryExpression() {
+            var floatToken = new Token(Float, "1.0", 1);
+            var tokens = new List<Token>() {
+                floatToken,
+                new Token(Star, "*", 1),
+                floatToken,
+                new Token(Plus, "+", 1),
+                floatToken,
+                new Token(EqualEqual, "==", 1),
+                floatToken,
+                new Token(Less, "<", 1),
+                floatToken
+            };
+
+            var mult = new Binary(new Literal(floatToken), new Literal(floatToken), new Token(Star, "*", 1));
+            var left = new Binary(mult, new Literal(floatToken), new Token(Plus, "+", 1));
+            var right = new Binary(new Literal(floatToken), new Literal(floatToken), new Token(Less, "<", 1));
+
+            var expected = new Binary(left, right, new Token(EqualEqual, "==", 1));
+
+            var parser = new Parser(tokens);
+            var actual = parser.Parse();
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
