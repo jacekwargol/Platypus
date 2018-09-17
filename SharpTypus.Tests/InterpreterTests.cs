563046318116402ec743fdb5b8db28789c2e0481
Jacek Wargol <jwargol@gmail.com>
2018-09-17 16:35:25 +0200

    evaluate float test

---
+++ SharpTypus.Tests/InterpreterTests.cs
@@ -0,0 +1,31 @@
+﻿using SharpTypus.Parsing;
+using SharpTypus.Parsing.Expressions;
+using static SharpTypus.Parsing.TokenType;
+using Xunit;
+using SharpTypus.Interpreting;
+
+namespace SharpTypus.Tests {
+    public class InterpreterTests {
+        [Fact]
+        public void CanIntepretInteger() {
+            var expr = new Literal(new Token(Integer, "1", 1));
+            var expected = 1;
+
+            var interpreter = new Interpreter();
+            var actual = interpreter.Visit(expr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanIntepretFloat() {
+            var expr = new Literal(new Token(Float, "1", 1));
+            var expected = 1.0;
+
+            var interpreter = new Interpreter();
+            var actual = interpreter.Visit(expr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
